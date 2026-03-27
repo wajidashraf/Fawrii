51809ac4fd164c6f4185679934ab0168f54eb645
wajidashraf <wajidashraf673@gmail.com>
2026-03-27 12:21:04 +0500

    start on ne branch for design changes

--- src/app/support/page.tsx
+++ src/app/support/page.tsx
@@ -4,14 +4,14 @@ import { ComingSoon } from "@/components/shared/ComingSoon";
 export const metadata: Metadata = {
   title: "Support & Help | Fawrii",
   description:
-    "Get help and support as a Fawrii tutor. We are here to assist you.",
+    "Get help and support as a Fawrii tutor. We are here to assist you. ",
 };
 
 export default function SupportPage() {
   return (
     <ComingSoon
       title="Support & Help"
-      description="Our support and help centre is being set up and will be available soon."
+      description="Our support & help centre is being set up and will be available soon."
     />
   );
 }
